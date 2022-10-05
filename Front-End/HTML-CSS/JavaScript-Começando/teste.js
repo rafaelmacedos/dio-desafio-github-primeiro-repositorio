e13978be3996f9d7811ae44f61cde2cdf4e2c0d2
rafaelmacedos <macedorafael2002@gmail.com>
2022-10-05 14:40:24 -0300

    Inclusão dos projetos Front-End feitos aqui na DIO

---
+++ Front-End/HTML-CSS/JavaScript-Começando/teste.js
@@ -0,0 +1,7 @@
+function returnEvenValues(array) {
+    for(let i = 0; i < array.length);
+        if(array[i] % 2 === -) {
+            evenNums.push(array[i]);
+        }
+        console.log(evenNums)
+}
